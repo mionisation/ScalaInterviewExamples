d0f4c11ebc8efb0074509d2e693243253e37927f
Michael Ion <mion@Michaels-MacBook-Pro.local>
2019-09-16 07:50:50 +0100

    Add array rotation example

---
+++ ArrayRotate.scala
@@ -0,0 +1,24 @@
+/*
+ Given: Array: arr, int: steps
+ Rotate arr steps amounts to the right
+*/
+
+import org.scalatest.{FunSuite, Matchers}
+
+class Testable extends FunSuite with Matchers {
+
+    def rotate_inplace[T](arr:List[T], steps: Int) = {
+        arr.slice(arr.length - steps, arr.length) ++ arr.slice(0, arr.length - steps)
+    }
+
+  test("compare lists") {
+    val array = List[Int](1, 2, 3)
+    val result = rotate_inplace[Int](array, 2)
+    val expected = List[Int](2, 3, 1)
+    assert(result === expected);
+  }    
+}
+
+object ArrayRotate extends App {
+  new Testable().execute()
+}
